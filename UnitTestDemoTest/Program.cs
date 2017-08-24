d4f413c5276385ebb2647f4be092ceea7a936d02
zhang1231231231 <625452708@qq.com>
2017-08-24 22:45:30 +0800

    添加项目文件。

---
+++ UnitTestDemoTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemoTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int Add(int pNum1, int pNum2)
+        {
+            return pNum1 + pNum2;
+        }
+    }
+}
---
+++ UnitTestDemoTestTests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDemoTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemoTest.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            int a = 100;
+            int b = 10;
+            Assert.AreEqual(Program.Add(a, b),110);
+            //Assert.Fail();
+        }
+    }
+}
